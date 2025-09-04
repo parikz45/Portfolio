0f805d5a804bdd88b6bd0ca1da7b1d562acf2d2e
Parikshit R <148047580+parikz45@users.noreply.github.com>
2025-09-04 09:57:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './Components/Contact';
 
 const App = () => {
   return (
-    <div className={styles.App}>
+    <div className="App" >
       <Navbar/>
       <Hero/>
       <About/>
